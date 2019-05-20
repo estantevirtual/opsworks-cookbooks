c10fdd86f9a8950c833bda33c230da690a95d683
Cezar Almeida <calmeida@estantevirtual.com.br>
2019-05-20 18:58:39 -0300

    - habilitando restart do puma temporariamente

--- puma/definitions/puma_upstart.rb
+++ puma/definitions/puma_upstart.rb
@@ -2,11 +2,11 @@
 define :puma_upstart do
   memory = node['memory']['total'].split('kb')[0].to_i * 1024
 
-  # execute 'puma_restart' do
-  #   cmd = node[:opsworks][:rails_stack][:restart_command]
-  #   Chef::Log.info("!!! executing puma restart with: #{cmd}")
-  #   command cmd
-  # end
+  execute 'puma_restart' do
+    cmd = node[:opsworks][:rails_stack][:restart_command]
+    Chef::Log.info("!!! executing puma restart with: #{cmd}")
+    command cmd
+  end
 
   template '/etc/init/puma.conf' do
     mode '0644'
