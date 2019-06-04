c5f56e1a9e209a822cf28415b2f630dd8a0d72a9
Cezar Almeida <calmeida@estantevirtual.com.br>
2019-06-04 14:49:04 -0300

    - alterando ordem das etapas

--- deploy/recipes/rails.rb
+++ deploy/recipes/rails.rb
@@ -2,6 +2,7 @@ include_recipe 'deploy'
 include_recipe 'puma::rails'
 
 node[:deploy].each do |application, deploy|
+  is_puma = node[:opsworks][:rails_stack][:puma].eql?(true)
 
   if deploy[:application_type] != 'rails'
     Chef::Log.debug("Skipping deploy::rails application #{application} as it is not a Rails app")
@@ -23,33 +24,31 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
+  puma_upstart do
+    user deploy[:user]
+    group 'www-data'
+    deploy deploy
+  end if is_puma
+
   opsworks_deploy do
     deploy_data deploy
     app application
   end
 
-  if node[:opsworks][:rails_stack][:puma].eql?(true)
-    puma_upstart do
-      user deploy[:user]
-      group 'www-data'
-      deploy deploy
-    end
+  template "#{deploy[:deploy_to]}/shared/config/puma.rb" do
+    Chef::Log.info("!!! deploy #{deploy}")
 
-    template "#{deploy[:deploy_to]}/shared/config/puma.rb" do
-      Chef::Log.info("!!! deploy #{deploy}")
-
-      mode '0644'
-      owner deploy[:user]
-      group 'www-data'
-      source 'puma.conf.erb'
-      cookbook 'puma'
-      notifies :run, 'execute[puma_restart]', :delayed
-      variables(
-        :deploy => deploy,
-        :application => application,
-        :node => node,
-        :environment => OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables]),
-      )
-    end
-  end
+    mode '0644'
+    owner deploy[:user]
+    group 'www-data'
+    source 'puma.conf.erb'
+    cookbook 'puma'
+    notifies :run, 'execute[puma_restart]', :delayed
+    variables(
+      :deploy => deploy,
+      :application => application,
+      :node => node,
+      :environment => OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables]),
+    )
+  end if is_puma
 end
