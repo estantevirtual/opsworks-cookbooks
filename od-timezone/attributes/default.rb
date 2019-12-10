44eae90185d42dc561004ed3f587ea43933de2ed
Carla Becker <carla.becker@outlook.com>
2019-12-10 12:03:39 -0300

    Altera o od-timezone

--- od-timezone/attributes/default.rb
+++ od-timezone/attributes/default.rb
@@ -1,3 +1,3 @@
-default[:timezone][:clock][:ZONE]="Brazil/East"
+default[:timezone][:clock][:ZONE]="America/Fortaleza"
 default[:timezone][:clock][:UTC]="true"
 default[:timezone][:ntpdate][:command]="/usr/sbin/ntpdate -u pool.ntp.br"
