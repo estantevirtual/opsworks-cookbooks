c5102c7c5cf7ba6ba2d3acd87e4cdea89bce3adf
ramonPimentel <ramonpimentel92@gmail.com>
2020-01-21 17:42:28 -0300

    crontab puma

--- puma/recipes/check.rb
+++ puma/recipes/check.rb
@@ -3,16 +3,11 @@ include_recipe 'puma'
 
 node[:deploy].each do |application, deploy|
   template '/usr/bin/check-puma.sh' do
-    owner deploy[:user]
-    group deploy[:group]
     mode '555'
     source 'check.sh.erb'
   end
 
-
   template '/etc/cron.d/check' do
-    owner deploy[:user]
-    group deploy[:group]
     mode '555'
     source 'crontab.erb'
   end
