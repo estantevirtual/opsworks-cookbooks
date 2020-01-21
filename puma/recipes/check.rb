a60b75abc9ae95881c781ecb932157ee829fc4b6
ramonPimentel <ramonpimentel92@gmail.com>
2020-01-21 17:58:52 -0300

    crontab puma

--- puma/recipes/check.rb
+++ puma/recipes/check.rb
@@ -1,18 +1,19 @@
-include_recipe 'nginx'
-include_recipe 'puma'
+include_recipe 'deployer'
 
-node[:deploy].each do |application, deploy|
-  template '/usr/bin/check-puma.sh' do
-    mode '555'
-    source 'check.sh.erb'
-  end
+template '/usr/bin/check-puma.sh' do
+  owner node['deployer']['user']
+  group node['deployer']['group']
+  mode '555'
+  source 'check.sh.erb'
+end
 
-  template '/etc/cron.d/check' do
-    mode '555'
-    source 'crontab.erb'
-  end
+template '/etc/cron.d/check' do
+  owner node['deployer']['user']
+  group node['deployer']['group']
+  mode '555'
+  source 'crontab.erb'
+end
 
-  execute 'setup node' do
-    command "sudo su - crontab /etc/cron.d/check"
-  end
+execute 'setup node' do
+  command "sudo su - crontab /etc/cron.d/check"
 end
