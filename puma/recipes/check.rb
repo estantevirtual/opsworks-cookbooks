b66018ac761989b9fdc1889f8596f33e33872f81
ramonPimentel <ramonpimentel92@gmail.com>
2020-01-22 13:51:24 -0300

    Remove group

--- puma/recipes/check.rb
+++ puma/recipes/check.rb
@@ -11,7 +11,6 @@ node[:deploy].each do |application, deploy|
     minute "*/2"
     hour "*"
     weekday "*"
-    group deploy[:group]
     user deploy[:user]
     command '/usr/bin/check-puma.sh'
   end
