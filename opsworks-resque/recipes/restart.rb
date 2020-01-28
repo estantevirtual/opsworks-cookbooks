11802c0d45c35be7989548b52c03fbb5c3fd6a38
Cezar Almeida <calmeida@estantevirtual.com.br>
2020-01-28 11:05:02 -0300

    adicionando sudo su no restart do resque

--- opsworks-resque/recipes/restart.rb
+++ opsworks-resque/recipes/restart.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |application, deploy|
-  execute "cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} bundle exec rake restart_resque > /dev/null"
+  execute "sudo su - deploy -c 'cd #{deploy[:deploy_to]}/current && RAILS_ENV=#{deploy[:rails_env]} bundle exec rake restart_resque > /dev/null'"
   
   service "resque-#{application}-scheduler" do
     supports :status => true, :restart => true, :start => true
