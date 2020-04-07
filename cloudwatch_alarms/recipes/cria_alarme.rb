37d4ab6fedd8454e3ab9c91b8ba2549d5b3cb5aa
Carla Becker <carla.becker@outlook.com>
2020-04-07 17:36:43 -0300

    Ajuste da receita cria_alarme

--- cloudwatch_alarms/recipes/cria_alarme.rb
+++ cloudwatch_alarms/recipes/cria_alarme.rb
@@ -1,3 +1,4 @@
-execute 'cria_alarme' do
-    command '/bin/bash wget https://57vhjlhdn7.execute-api.us-east-1.amazonaws.com/dev'
+http_request 'cria_alarme' do
+    url 'https://57vhjlhdn7.execute-api.us-east-1.amazonaws.com/dev'
+    action :get
 end
