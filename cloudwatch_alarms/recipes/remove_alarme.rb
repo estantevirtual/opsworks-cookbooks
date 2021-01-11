d99ec9d1ac49794efbfc2ffcc77017d72eab1436
cahbecker <carla.becker@outlook.com>
2021-01-11 16:26:06 -0300

    ajusta url de invoke dos lambdas de alarmes

--- cloudwatch_alarms/recipes/remove_alarme.rb
+++ cloudwatch_alarms/recipes/remove_alarme.rb
@@ -1,4 +1,4 @@
 http_request 'remove_alarme' do
-    url 'https://lambda.estantevirtual.com.br/remove-alarme'
+    url 'https://bjac6lyhl9.execute-api.us-east-1.amazonaws.com/prod/'
     action :get
 end
