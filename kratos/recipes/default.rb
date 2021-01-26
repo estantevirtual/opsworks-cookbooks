032d7bab3d2308cf92363f498cac5f9ff27fc5c6
cahbecker <carla.becker@outlook.com>
2021-01-26 13:40:34 -0300

    cria o diretorio certificates

--- kratos/recipes/default.rb
+++ kratos/recipes/default.rb
@@ -3,6 +3,7 @@ bash 'include_certificate' do
     code <<-EOH
       export AWS_ACCESS_KEY_ID=#{node['credentials']['aws_access_key_id']}
       export AWS_SECRET_ACCESS_KEY=#{node['credentials']['aws_secret_access_key']}
+      mkdir -p #{node['app_path']}
       /usr/bin/aws s3 cp #{node['cert_path']} #{node['app_path']}
       /usr/bin/aws s3 cp #{node['key_path']} #{node['app_path']}
     EOH
