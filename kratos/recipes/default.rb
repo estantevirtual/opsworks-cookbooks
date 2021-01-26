4dd1cf4876878db9c742e5b223ae403d1194da47
cahbecker <carla.becker@outlook.com>
2021-01-26 13:09:38 -0300

    inclui comandos para adicionar o certificado

--- kratos/recipes/default.rb
+++ kratos/recipes/default.rb
@@ -1,7 +1,9 @@
-# exporta variaveis das credenciais de acesso 
-bash 'set_env_var' do
+# inclui o certificado e a key no diretorio correto da aplicacao
+bash 'include_certificate' do
     code <<-EOH
-      export AWS_ACCESS_KEY_ID=<%= node.credentials.aws_access_key_id %> && \
+      export AWS_ACCESS_KEY_ID=<%= node.credentials.aws_access_key_id %>
       export AWS_SECRET_ACCESS_KEY=<%= node.credentials.aws_secret_access_key %>
+      /usr/bin/aws s3 cp <%= node.cert_path %> <%= node.app_path %>
+      /usr/bin/aws s3 cp <%= node.key_path %> <%= node.app_path %>
     EOH
 end
